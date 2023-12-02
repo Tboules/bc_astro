a58bbd48161526ddd64b941b8c309b7123aaf0d0
Tboules <tboules@gmail.com>
2023-12-01 16:09:56 -0800

    header breakpoints

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -44,11 +44,11 @@ const NAV_SCHEMA: INavSchema = {
 export default function MainHeader({ children }: React.PropsWithChildren) {
   return (
     <>
-      <div className="h-16" />
-      <div className="w-screen bg-background/60 backdrop-blur h-16 border-b-border border-b fixed top-0 left-0">
-        <div className="max-w-screen-xl h-full m-auto flex justify-between items-center">
+      <div className="w-full bg-background/60 backdrop-blur h-16 border-b-border border-b sticky z-50 top-0 left-0">
+        <div className="max-w-screen-xl h-full m-auto flex justify-between items-center px-2">
           {children}
-          <h1>menu</h1>
+          <h1 className="md:hidden">menu</h1>
+          <h1 className="hidden md:inline">big menu</h1>
         </div>
       </div>
     </>
