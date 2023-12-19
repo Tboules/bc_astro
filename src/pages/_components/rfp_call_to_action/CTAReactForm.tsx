c0b52023dd740d69e50b2a791998a04593037fe7
Tboules <tboules@gmail.com>
2023-12-19 15:37:46 -0800

    adding finishing touches on form

--- src/pages/_components/rfp_call_to_action/CTAReactForm.tsx
+++ src/pages/_components/rfp_call_to_action/CTAReactForm.tsx
@@ -1,11 +1,42 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type FormData = {
+  email: string;
+};
 
 export default function CTAReactForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(
+      z.object({
+        email: z.string().email(),
+      }),
+    ),
+  });
+
+  const onSubmit = handleSubmit((d) => {
+    console.log(d);
+    reset();
+  });
+
   return (
-    <form className="flex flex-col md:flex-row gap-2 pt-2 md:pt-6 md:gap-4 ">
-      <Input placeholder="email" type="text" />
-      <Button>Get Your Template</Button>
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col md:flex-row gap-2 pt-2 md:pt-6 md:gap-4 "
+    >
+      <div className="w-full">
+        <Input {...register("email")} placeholder="email" type="text" />
+        <p className="text-red-400 py-2">{errors.email?.message}</p>
+      </div>
+      <Button type="submit">Get Your Template</Button>
     </form>
   );
 }
