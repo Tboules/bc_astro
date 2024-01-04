e58ad138d595ec7b5425c1783c1647b02e4775cb
Tboules <tboules@gmail.com>
2024-01-04 13:31:00 -0800

    feature: api test vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,4 +15,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+  output: "hybrid",
+  adapter: vercel(),
 });
+
