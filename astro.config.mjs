ab7e038f8dfe475f4482693dd955d747118d8fa6
Tboules <tboules@gmail.com>
2024-01-16 14:31:46 -0800

    function per route

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
   output: "hybrid",
   adapter: vercel({
     includeFiles: ["./public/MCF.pdf"],
+    functionPerRoute: true,
   }),
   image: {
     domains: ["images.ctfassets.net"],
